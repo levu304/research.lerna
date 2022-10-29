b1c855ab7eb26ddb12428401c0ad05389125f4c6
levu304 <ln-anhvu@vn.pasonatech.co.jp>
2022-10-29 22:14:46 +0700

    update

--- packages/express.postgres/src/controllers/file.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-declare function uploadFile(req: Request, res: Response): Promise<void>;
-declare function uploadFiles(req: Request, res: Response): Promise<void>;
-declare function downloadFile(req: Request, res: Response): Promise<void>;
-export { uploadFile, uploadFiles, downloadFile };
